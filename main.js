770b65422f9dfa84d21a242e4f6163780d7b5ad9
Foxbond <foxbondpl@gmail.com>
2014-11-17 20:52:09 +0100

    new solution fix

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ var io = require('socket.io')(http);
 var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
+    io = require('socket.io').listen(server);
 
 server.listen(process.env.PORT || 5000);
 
