af242e7f98a25509d29be0e0bec99507c5555790
Foxbond <foxbondpl@gmail.com>
2014-11-17 21:30:20 +0100

    new clean

--- main.js
+++ main.js
@@ -1,44 +1,23 @@
-/*
-var app = require('express')();
+var express = require('express');
+var http = require('http');
+var sio = require('socket.io');
 
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
-	res.header("Access-Control-Allow-Headers", "Content-Type");
-	res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-    next();
-});
-
-var http = require('http').Server(app);
-
-var port = process.env.PORT || 5000;
-
-http.listen(port, function(){
-  console.log('listening on *:'+port);
-});
+var app = express();
+var server = http.createServer(app);
+var io = sio.listen(server);
 
 
-
-
-var io = require('socket.io')(http);
-*************/
-
-
-var express = require('express'),
-    app = express(),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server);
 
 server.listen(process.env.PORT || 80, function (){
 	var addr = server.address();
-  console.log('   app listening '+JSON.stringify(addr));
+	console.log('App listening '+JSON.stringify(addr));
 });
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.sockets.on('connection', function(socket){
+io.on('connection', function(socket){
 console.log("user connected");
 
   socket.on('chat message', function(msg){
