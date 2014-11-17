a51ce90e20057d285a67832462f521fc3897970e
Foxbond <foxbondpl@gmail.com>
2014-11-17 20:41:51 +0100

    fix #3

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
 var app = require('express')();
+
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.header("Access-Control-Allow-Headers", "Content-Type");
+	res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
+});
+
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendfile(__dirname + '/index.html');
-});
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
-
-
-io.set("origins","*");
 var port = process.env.PORT || 5000;
 
 http.listen(port, function(){
   console.log('listening on *:'+port);
 });
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+console.log("user connected");
+
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
