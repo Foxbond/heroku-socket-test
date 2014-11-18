6c3a96f750b0e9ed4ddeeac511918ab80cd3d4cc
Foxbond <foxbondpl@gmail.com>
2014-11-18 17:11:33 +0100

    old but rewritten solution

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
+var util = require('util');
+
 var express = require('express');
 var http = require('http');
 var sio = require('socket.io');
 
 var app = express();
+
+var allowCrossDomain = function(req, res, next) {
+	console.log(util.inspect(req));
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS');
+    res.header('Access-Control-Allow-Headers', '*');
+	res.header('Access-Control-Max-Age', 3628800);
+	return next();
+}; 
+
+app.use(allowCrossDomain);
+
 var server = http.createServer(app);
 var io = sio.listen(server);
 
@@ -17,17 +32,7 @@ server.listen(process.env.PORT || 80, function (){
 });
 
 
-var allowCrossDomain = function(req, res, next) {
-	console.log(req.header("Origin"));
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS');
-    res.header('Access-Control-Allow-Headers', '*');
-	res.header('Access-Control-Max-Age', 3628800);
-	return next();
-}; 
 
-app.use(allowCrossDomain);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
